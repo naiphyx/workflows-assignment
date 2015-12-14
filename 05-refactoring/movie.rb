41eba07f201b3c6b97ecaed4abe17edac6e2511c
Viktoria Steiner <vsteiner.mmt-b2014@fh-salzburg.ac.at>
2015-12-14 15:53:19 +0100

    finalization of refactoring

--- 05-refactoring/movie.rb
+++ 05-refactoring/movie.rb
@@ -4,7 +4,7 @@ class Movie
   CHILDRENS = 2
 
   attr_reader :title
-  attr_accessor :price_code
+  attr_reader :price_code
 
   def price_code=(value)
     @price_code = value
@@ -27,23 +27,40 @@ class Movie
   end
 
   def frequent_renter_points(days_rented)
-    (price_code == NEW_RELEASE && days_rented > 1) ? 2 : 1
+    @price.frequent_renter_points(days_rented)
   end
 end
 
+module DefaultPrice
+  def frequent_renter_points(days_rented)
+    1
+  end
+end
+
+
 class RegularPrice
+  include DefaultPrice
+
   def charge(days_rented)
     result = 2
     result += (days_rented - 2) * 1.5 if days_rented > 2
     result
   end
 end
+
 class NewReleasePrice
   def charge(days_rented)
     days_rented * 3
   end
+
+  def frequent_renter_points(days_rented)
+    days_rented > 1 ? 2 : 1
+  end
 end
+
 class ChildrensPrice
+  include DefaultPrice
+
   def charge(days_rented)
     result = 1.5
     result += (days_rented - 3) * 1.5 if days_rented > 3
